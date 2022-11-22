2461dc3e35e7ee7f652c9d2d270611bad82182be
dannyvang <dannyvangpro@gmail.com>
2022-11-22 15:10:58 +0100

    erreur bateau service

--- src/app/bateaux/bateaux.page.ts
+++ src/app/bateaux/bateaux.page.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, NavigationExtras } from "@angular/router";
+import { Bateau } from "../models/bateau";
+import { BateauService } from "../services/bateau";
 
 @Component({
-  selector: 'app-bateaux',
-  templateUrl: './bateaux.page.html',
-  styleUrls: ['./bateaux.page.scss'],
+  selector: "app-bateaux",
+  templateUrl: "./bateaux.page.html",
+  styleUrls: ["./bateaux.page.scss"],
 })
 export class BateauxPage implements OnInit {
+  bateauList!: Bateau[];
 
-  constructor() { }
+  constructor(private router: Router, private bateauService: BateauService) {}
 
   ngOnInit() {
+    this.bateauList = this.bateauService.bateauList.slice();
   }
 
+  onLoadBateau(name: Bateau) {
+    let NavigationExtras: NavigationExtras = {
+      state: {
+        boat: name,
+      },
+    };
+    this.router.navigate(["/single-bateaux"], NavigationExtras);
+  }
 }
--- src/app/recette/recette.page.ts
+++ src/app/recette/recette.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router,NavigationExtras } from '@angular/router';
+import { Restaurant } from '../models/restaurant';
+
 
 @Component({
   selector: 'app-recette',
   templateUrl: './recette.page.html',
   styleUrls: ['./recette.page.scss'],
 })
-export class RecettePage implements OnInit {
 
-  constructor() { }
+export class RecettePage implements OnInit {
+  restaurantList!: Restaurant[];
+
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    this.
   }
 
 }
